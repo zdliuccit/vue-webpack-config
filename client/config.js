248a5a9a699c6c6f6687b4148931c97580d9f8ed
刘正东 <liuzhengdong@xkeshi.com>
2017-09-14 17:56:28 +0800

    代理中间件完善

--- client/config.js
+++ client/config.js
@@ -1,5 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import http from './../config/utils/http'
+import http from '@config/utils/http'
+import { addRequestInterceptor, addResponseInterceptor } from '@config/utils/http/http'
+
 Vue.use(http)
 Vue.use(VueRouter)
+
+// 接口前自动添加api配置
+addRequestInterceptor(
+  (config) => {
+    config.url = `/api${config.url}`
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+// 返回response前处理
+addResponseInterceptor(
+  (response) => {
+    // 在这里统一前置处理请求响应
+    if (Number(response.data.code) !== 0) {
+      // 全局notify有问题，还是自己处理吧
+      return Promise.reject(response.data)
+    }
+    return Promise.resolve(response.data)
+  },
+  (error) => {
+    // 401直接跳转到登陆页面
+    if (!IS_DEBUG && Number(error.response.status) === 401) {
+    }
+    return Promise.reject(error || '出错了')
+  }
+)
--- config/utils/proxyMiddleWare.js
+++ config/utils/proxyMiddleWare.js
@@ -8,11 +8,11 @@ const compose = require('koa-compose')
 const appConfig = require('./../../app.config')
 const { IS_DEBUG, IS_SERVER_PRODUCTION } = require('./env')
 
-// const needToken = !!appConfig.token
-// let tokenManager
-// if (needToken) {
-//   tokenManager = require('./token')(appConfig.token)
-// }
+const needToken = !!appConfig.token
+let tokenManager
+if (needToken) {
+  tokenManager = require('./token')(appConfig.token)
+}
 
 /**
  * 获取代理配置
@@ -32,56 +32,27 @@ function getProxyConfig () {
  * 代理处理中间件
  * @return {Function} koa middleware
  */
-module.exports = function (/* opts */) {
-
+module.exports = function () {
   async function preProxyMiddleware (ctx, next) {
     const url = ctx.url
     let proxyTarget
     let proxyConfig = getProxyConfig()
-    debugger
-    // 非生产环境，才允许后端进行调试
-    if (!IS_SERVER_PRODUCTION) {
-      // 如果请求头里带了`proxyconfig`(node拿到的都是小写)字段,则将其与项目代理配置合并,用于调试
-      // Proxy-Config这个头字段居然被chrome拒绝设置...
-      let headerProxyConfig = ctx.headers.proxyconfig
-      if (headerProxyConfig) {
-        console.log(`'proxyconfig' detected in headers: '${headerProxyConfig}'`)
-        try {
-          headerProxyConfig = JSON.parse(headerProxyConfig)
-        } catch (e) {
-          console.log('proxyconfig parsing FAILED, please check it.')
-          headerProxyConfig = {}
-        }
-        // Object.assign还是尽量要传三个参数啊（第一个{}），又犯了一个低级错误。。。
-        proxyConfig = Object.assign({}, proxyConfig, headerProxyConfig)
-        console.log(`Merged proxy config is: ${JSON.stringify(proxyConfig)}`)
-      }
-    }
     // 在appConfig.proxy中寻找匹配前缀的代理
     for (const [prefix, target] of Object.entries(proxyConfig)) {
       if (url.startsWith(prefix)) {
-        // 代理目标可能有这么几种写法:
-        // 1.http://192.168.xxx.xxx:dddd
-        // 2.http://10.14.xxx.xx:dddd
-        // 3.http://xx.xkeshi.so
-        // 4.http://jenkins-name
-        // 但是, 目前本地环境只有1，3两种写法
-        // 为了解决多重nodejs代理的问题，如果代理目标是域名形式，则不替换前缀
-        // 默认生产模式，不会设置代理为域名形式
-        // 注意本地如果启动生产模式，并且代理目标如果是域名形式，那么仍然会有此问题，
-        // 鉴于此种情况非常少见，就不在这里处理了
-        // 只有本地开发模式，才会执行后面的域名判断
-        if (!IS_DEBUG || !isDomainTarget(target)) {
+        // 匹配替换
+        if (!IS_DEBUG) {
           ctx.url = url.replace(prefix, '')
         }
         proxyTarget = target
         ctx._proxyTarget = proxyTarget
-        console.log(`Proxy Found: '${prefix}' => '${proxyTarget}'`)
+
+        console.log(`Match to proxy: '${prefix}' => '${proxyTarget}'`)
         break
       }
     }
     if (!proxyTarget) {
-      console.log('Proxy NOT FOUND, skipped')
+      console.log('Proxy not found, skipped')
       return Promise.resolve()
     }
     console.log(`Request '${url}' will be proxied to '${proxyTarget + ctx.url}'`)
@@ -133,8 +104,7 @@ module.exports = function (/* opts */) {
       async userResDecorator(proxyRes, proxyResData, ctx) {
         console.log('ProxyRes headers:', JSON.stringify(ctx.response.headers))
         const location = `${ctx._proxyTarget}${ctx.url}`
-        console.log(
-          `Proxy request '${location}' completed(${proxyRes.statusCode}), costing ${Date.now() - ctx._proxyStartTimestamp}ms.`)
+        console.log(`Proxy request '${location}' completed(${proxyRes.statusCode}), costing ${Date.now() - ctx._proxyStartTimestamp}ms.`)
         if (!needToken) {
           return proxyResData
         }
