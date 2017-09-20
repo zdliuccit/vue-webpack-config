8af81946f70a76d0bab1eec0252809ebb50ea009
zdliuccit <335758663@qq.com>
2017-09-20 14:15:32 +0800

    no message

--- client/view/index.vue
+++ client/view/index.vue
@@ -15,7 +15,7 @@
       sendGet() {
         this.http.get('/v3/assistant/coordinate/convert?key=ff0bcf778c5eeb93bd8b068b6e3f7781&locations=116.481499,39.990475|116.481499,39.990375&output=JSON')
           .then((resp) => {
-            console.log(resp)
+              console.log(resp)
           })
           .catch(() => {
             console.log('error............')
