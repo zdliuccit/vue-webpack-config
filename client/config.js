19dbf78d4fcebfffd730d881af6fb5d5e22da0da
刘正东 <liuzhengdong@xkeshi.com>
2017-09-23 19:06:04 +0800

    添加vue-logger日志插件

--- client/config.js
+++ client/config.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 引入http请求插件
 import http from '@config/utils/http'
+// 引入log日志插件
+import vueLogger from '@config/utils/logger/vue-logger'
 import { addRequestInterceptor, addResponseInterceptor } from '@config/utils/http/http'
 
+// 注册插件
 Vue.use(http)
+Vue.use(vueLogger)
 Vue.use(VueRouter)
 
 // request前自动添加api配置
