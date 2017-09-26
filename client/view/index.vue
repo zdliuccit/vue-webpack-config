444bfe32960e170a75b07d4ad9f4896be0df38db
刘正东 <liuzhengdong@xkeshi.com>
2017-09-26 16:00:18 +0800

    测试 pre-commit

--- client/view/index.vue
+++ client/view/index.vue
@@ -32,7 +32,7 @@
         this.info('开始请求')
         this.warn('开始请求')
         this.error('开始请求')
-        let ss21
+        console.log(11)
         this.result = ''
         this.http.get(this.testUrl)
           .then((resp) => {
