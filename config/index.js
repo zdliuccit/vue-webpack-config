7fd7da3ef2fe208e025285242e4e0a377644ccca
zdliuccit <335758663@qq.com>
2017-11-15 10:14:55 +0800

    去除console

--- config/index.js
+++ config/index.js
@@ -2,7 +2,6 @@
  * Created by liuzhengdong on 2017/9/6.
  *  * app入口,使整个应用基于es6开发
  */
-console.log(process.env.NODE_ENV)
 require('babel-core/register')
 require('babel-polyfill')
 if (process.env.NODE_ENV === 'development') {
