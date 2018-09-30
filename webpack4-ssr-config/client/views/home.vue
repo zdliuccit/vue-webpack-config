ec474e24c843ce163ea733e9b05c09e791a3727c
zdliuccit <335758663@qq.com>
2018-09-30 10:16:35 +0800

    feat: 更新demo

--- webpack4-ssr-config/client/views/home.vue
+++ webpack4-ssr-config/client/views/home.vue
@@ -4,9 +4,9 @@
     <div class="demo-warp">
       <div class="img-logo"></div>
       <div class="demo-js">
-        <p><a href="https://koa.bootcss.com" target="_blank" title="Koa 2.5.2">Koa 2.5.2</a></p>
+        <p><a href="https://koa.bootcss.com" target="_blank" title="Koa 2.5.2">Koa 2.5.3</a></p>
         <p><a href="https://vuejs.org" target="_blank" title="Vue 2.5.17">Vue 2.5.17</a></p>
-        <p><a href="https://webpack.github.io" target="_blank" title="Webpack 4.16.5">Webpack 4.16.5</a></p>
+        <p><a href="https://webpack.github.io" target="_blank" title="Webpack 4.16.5">Webpack 4.20.2</a></p>
       </div>
       <ul class="func-ul">
         <li><a @click="$router.push('/test/data')" href="javascrip:void(0);">Vuex数据处理</a></li>
