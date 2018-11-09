38665461ba16c73303f2780a36fbd4e6b89ba3d9
zdliuccit <335758663@qq.com>
2018-11-09 13:33:13 +0800

    feat: 更新配置

--- webpack4-ssr-config/app.config.js
+++ webpack4-ssr-config/app.config.js
@@ -3,12 +3,13 @@
  */
 const path = require('path')
 const constants = require('./constants')
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // 主服务启动端口
   appPort: 8088,
   // 代理配置，可支持多个代理，key为前缀，命中后，会把前缀去掉，转发到代理服务器
-  proxy: constants.proxy,
+  proxy: isProd ? constants.prodProxy : constants.proxy,
   // webpack的差异化配置
   webpack: {
     entry: {
--- webpack4-ssr-config/constants.js
+++ webpack4-ssr-config/constants.js
@@ -3,6 +3,10 @@
  */
 module.exports = {
   proxy: {
-    '/api': 'http://restapi.amap.com',
+    '/api': 'http://10.200.92.98:7001',
+    // '/api': 'http://192.168.199.177:7001',
   },
+  prodProxy: {
+    '/api': 'http://172.16.119.198:7001',
+  }
 }
