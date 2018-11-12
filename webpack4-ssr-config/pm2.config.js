e34c8c5edb7c611aca29528293d6b1777c892ef2
zdliuccit <335758663@qq.com>
2018-11-12 15:09:41 +0800

    no message

--- webpack4-ssr-config/pm2.config.js
+++ webpack4-ssr-config/pm2.config.js
@@ -5,14 +5,16 @@ module.exports = {
     args: '', // 由传递给脚本的参数组成的字符串或字符串数​​组。
     output: './log/out.log',
     error: './log/error.log',
+    log: './log/combined.outerr.log',
     merge_logs: true, // 集群的所有实例的日志文件合并
-    log_date_format: "DD-MM-YYYY HH:MM:SS",
+    log_date_format: "DD-MM-YYYY",
     instances: 4, // 进程数 1、数字 2、'max'根据cpu内核数
     max_memory_restart: '1G', // 当内存超过1024M时自动重启
+    watching: true,
     env_test: {
       NODE_ENV: 'production'
     },
-    env_prod: {
+    env_production: {
       NODE_ENV: 'production'
     }
   }],
